dfdc4b0399a7627d6d244f19bced7ff73b5cf2b2
Leo_Hsu <zoro061181249@gmail.com>
2023-07-22 22:39:55 +0800

    1.改善login頁面UI 2.加入返回首頁功能

--- Restaurant_Init/Models/ViewModels/LoginViewModel.cs
+++ Restaurant_Init/Models/ViewModels/LoginViewModel.cs
@@ -3,10 +3,13 @@ namespace Restaurant_Init.Models.ViewModels
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "請輸入使用者名稱")]
+        [Required(ErrorMessage = "請輸入使用者帳號")]
+        //顯示名稱
+        [Display(Name = "登入帳號")]
         public string UserId { get; set; }
 
         [Required(ErrorMessage = "請輸入密碼")]
+        [Display(Name = "登入密碼")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
